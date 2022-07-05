bcf9c4ddcdce0f6fd0a08e53ec86daf0840f51fa
dassshha <dzhguleva@mail.ru>
2022-07-05 22:11:42 +0300

    add mocks

---
+++ project/src/mocks/offers.ts
@@ -0,0 +1,141 @@
+import {OffersList} from '../types/offers-list';
+
+export const offers: OffersList = [
+  {
+    'bedrooms': 3,
+    'city': {
+      'location': {
+        'latitude': 52.370216,
+        'longitude': 4.895168,
+        'zoom': 10
+      },
+      'name': 'Amsterdam'
+    },
+    'description': 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    'goods': ['Heating', 'Kitchen', 'Cable TV', 'Washing machine', 'Coffee machine', 'Dishwasher'],
+    'host': {
+      'avatarUrl': '../../public/img/amsterdam.jpg',
+      'id': 3,
+      'isPro': true,
+      'name': 'Angelina'
+    },
+    'id': 1,
+    'images': ['img/1.png', 'img/2.png'],
+    'isFavorite': false,
+    'isPremium': false,
+    'location': {
+      'latitude': 52.35514938496378,
+      'longitude': 4.673877537499948,
+      'zoom': 8
+    },
+    'maxAdults': 4,
+    'previewImage': 'https://st.depositphotos.com/2288675/2455/i/450/depositphotos_24553989-stock-photo-hotel.jpg',
+    'price': 120,
+    'rating': 4.8,
+    'title': 'Beautiful & luxurious studio at great location',
+    'type': 'apartment'
+  },
+  {
+    'bedrooms': 3,
+    'city': {
+      'location': {
+        'latitude': 52.370216,
+        'longitude': 4.895168,
+        'zoom': 10
+      },
+      'name': 'Amsterdam'
+    },
+    'description': 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    'goods': ['Heating', 'Kitchen', 'Cable TV', 'Washing machine', 'Coffee machine', 'Dishwasher'],
+    'host': {
+      'avatarUrl': '../../public/img/amsterdam.jpg',
+      'id': 3,
+      'isPro': true,
+      'name': 'Angelina'
+    },
+    'id': 2,
+    'images': ['img/1.png', 'img/2.png'],
+    'isFavorite': true,
+    'isPremium': true,
+    'location': {
+      'latitude': 52.35514938496378,
+      'longitude': 4.673877537499948,
+      'zoom': 8
+    },
+    'maxAdults': 4,
+    'previewImage': 'https://bestvietnam.ru/wp-content/uploads/2019/04/отель-1.jpg',
+    'price': 80,
+    'rating': 3.2,
+    'title': 'Good hotel!',
+    'type': 'hotel'
+  },
+  {
+    'bedrooms': 3,
+    'city': {
+      'location': {
+        'latitude': 52.370216,
+        'longitude': 4.895168,
+        'zoom': 10
+      },
+      'name': 'Amsterdam'
+    },
+    'description': 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    'goods': ['Heating', 'Kitchen', 'Cable TV', 'Washing machine', 'Coffee machine', 'Dishwasher'],
+    'host': {
+      'avatarUrl': '../../public/img/amsterdam.jpg',
+      'id': 3,
+      'isPro': true,
+      'name': 'Angelina'
+    },
+    'id': 3,
+    'images': ['img/1.png', 'img/2.png'],
+    'isFavorite': true,
+    'isPremium': false,
+    'location': {
+      'latitude': 52.35514938496378,
+      'longitude': 4.673877537499948,
+      'zoom': 8
+    },
+    'maxAdults': 4,
+    'previewImage': 'https://s0.rbk.ru/v6_top_pics/media/img/0/23/755615312255230.jpg',
+    'price': 20,
+    'rating': 1.7,
+    'title': 'Omg!Dangerous',
+    'type': 'hostel'
+  },
+  {
+    'bedrooms': 3,
+    'city': {
+      'location': {
+        'latitude': 52.370216,
+        'longitude': 4.895168,
+        'zoom': 10
+      },
+      'name': 'Amsterdam'
+    },
+    'description': 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    'goods': ['Heating', 'Kitchen', 'Cable TV', 'Washing machine', 'Coffee machine', 'Dishwasher'],
+    'host': {
+      'avatarUrl': '../../public/img/amsterdam.jpg',
+      'id': 3,
+      'isPro': true,
+      'name': 'Angelina'
+    },
+    'id': 4,
+    'images': ['img/1.png', 'img/2.png'],
+    'isFavorite': false,
+    'isPremium': true,
+    'location': {
+      'latitude': 52.35514938496378,
+      'longitude': 4.673877537499948,
+      'zoom': 8
+    },
+    'maxAdults': 4,
+    'previewImage': 'https://reghotel.com/wp-content/uploads/2019/09/krasivie.jpg',
+    'price': 500,
+    'rating': 4.9,
+    'title': 'Luxury hotel with a lot of food',
+    'type': 'hotel'
+  }
+];
+
