0e2b1138fc87165522e3582868bb2e1632a040ac
dassshha <dzhguleva@mail.ru>
2022-07-05 22:13:30 +0300

    add comment component

---
+++ project/src/components/rating-star/rating-star.tsx
@@ -0,0 +1,21 @@
+import {Fragment} from 'react';
+
+type RatingStarProps = {
+  value: number,
+  title: string,
+}
+
+function RatingStar({value, title}: RatingStarProps): JSX.Element {
+  return (
+    <Fragment>
+      <input className="form__rating-input visually-hidden" name="rating" value={value} id={`${value}-stars`} type="radio"/>
+      <label htmlFor={`${value}-stars`} className="reviews__rating-label form__rating-label" title={title}>
+        <svg className="form__star-image" width="37" height="33">
+          <use xlinkHref="#icon-star"></use>
+        </svg>
+      </label>
+    </Fragment>
+  );
+}
+
+export {RatingStar};
