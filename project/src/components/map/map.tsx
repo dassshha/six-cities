fb592acc46458b627383f3dac88271b44892eca4
dassshha <dzhguleva@mail.ru>
2022-07-18 23:35:55 +0300

    add leaflet styles

--- project/src/components/map/map.tsx
+++ project/src/components/map/map.tsx
@@ -3,6 +3,7 @@ import {CityType} from '../../types/city-type';
 import {OffersListType} from '../../types/offers-list-type';
 import {Icon} from 'leaflet';
 import {URL_MARKER_CURRENT, URL_MARKER_DEFAULT} from '../../const';
+import 'leaflet/dist/leaflet.css';
 
 type MapProps = {
   city: CityType,
@@ -29,19 +30,20 @@ type UpdateMapCenterProps = {
 function UpdateMapCenter({city}: UpdateMapCenterProps): null {
   const map = useMap();
   map.panTo([city.location.latitude, city.location.longitude]);
+  map.invalidateSize(true);
   return null;
 }
 
 function Map({city, points, selectedPoint}: MapProps): JSX.Element {
   return (
-    <MapContainer center={[city.location.latitude, city.location.longitude]} zoom={city.location.zoom} scrollWheelZoom={false} style={{height: '500px'}}>
-      <UpdateMapCenter city={city}/>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-        url='https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png'
-      />
-      {points.map((point) => <Marker position={[point.location.latitude, point.location.longitude]} key={point.id} icon={selectedPoint !== undefined && point.id === selectedPoint ? currentCustomIcon : defaultCustomIcon}/>)}
-    </MapContainer>
+      <MapContainer center={[city.location.latitude, city.location.longitude]} zoom={city.location.zoom} scrollWheelZoom={false} style={{height: '100%'}} >
+        <UpdateMapCenter city={city}/>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+          url='https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png'
+        />
+        {points.map((point) => <Marker position={[point.location.latitude, point.location.longitude]} key={point.id} icon={selectedPoint !== undefined && point.id === selectedPoint ? currentCustomIcon : defaultCustomIcon}/>)}
+      </MapContainer>
   );
 }
 
